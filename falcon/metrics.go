ad2d2803d094f8322211c2dee946bd06ed2f09e1
roundshot <133510447+roundshot@users.noreply.github.com>
2023-03-11 17:01:14 +0000

    Update scala-library to 2.12.14

--- falcon/metrics.go
+++ falcon/metrics.go
@@ -96,3 +96,63 @@ func isAliveness(s string) int64 {
 func partitions(s []string) int64 {
 	switch len(s) {
 	case 0:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func consumerUtil(c interface{}) float64 {
+	if vv, ok := c.(float64); ok {
+		return trimFloat(vv * 100.00)
+	} else if _, ok := c.(bool); ok {
+		return 0.0
+	} else if _, ok := c.(string); ok {
+		return 0.0
+	}
+	return 0.0
+}
+
+func updateCurrentStatsDB(db string) {
+	statsDB.SetCurrentLocate(db)
+}
+
+// GetCurrentStatsDB get current stats management database
+func GetCurrentStatsDB() *g.StatsDB {
+	return statsDB
+}
+
+// handleJudge
+func handleJudge() (data []*MetaData) {
+	data = make([]*MetaData, 0)
+	nd, err := funcs.GetNode()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	data = append(data, NewMetric(overviewPrefix+"ioReadawait", nd.Rawait, ""))    // io_read_avg_wait_time
+	data = append(data, NewMetric(overviewPrefix+"ioWriteawait", nd.Wawait, ""))   // io_write_avg_wait_time
+	data = append(data, NewMetric(overviewPrefix+"ioSyncawait", nd.Syncawait, "")) // io_sync_avg_wait_time
+	data = append(data, NewMetric(overviewPrefix+"memConnreader", nd.ConnectionReaders, ""))
+	data = append(data, NewMetric(overviewPrefix+"memConnwriter", nd.ConnectionWriters, ""))
+	data = append(data, NewMetric(overviewPrefix+"memConnchannels", nd.ConnectionChannels, ""))
+	data = append(data, NewMetric(overviewPrefix+"memMnesia", nd.Mnesia, ""))
+	data = append(data, NewMetric(overviewPrefix+"memMgmtdb", nd.MgmtDB, ""))
+	data = append(data, NewMetric(overviewPrefix+"memPlugins", nd.Plugins, ""))
+	data = append(data, NewMetric(overviewPrefix+"memMsgidx", nd.MsgIndex, ""))
+	data = append(data, NewMetric(overviewPrefix+"memBinary", nd.Binary, ""))
+	data = append(data, NewMetric(overviewPrefix+"memAlarm", nd.MemAlarmStatus(), ""))
+	data = append(data, NewMetric(overviewPrefix+"diskAlarm", nd.DiskAlarmStatus(), ""))
+	data = append(data, NewMetric(overviewPrefix+"fdUsedPct", calcPercentage(nd.FdUsed, nd.FdTotal), ""))
+	data = append(data, NewMetric(overviewPrefix+"memUsedPct", calcPercentage(nd.MemUsed, nd.MemLimit), ""))
+	data = append(data, NewMetric(overviewPrefix+"socketUsedPct", calcPercentage(nd.SocketsUsed, nd.SocketsTotal), ""))
+	data = append(data, NewMetric(overviewPrefix+"erlProcsUsedPct", calcPercentage(nd.ErlProcUsed, nd.ErlProcTotal), "")) //消费生产比
+	data = append(data, NewMetric(overviewPrefix+"runQueue", nd.RunQueues, ""))
+	data = append(data, NewMetric(overviewPrefix+"isPartition", partitions(nd.Partitions), "")) // 是否发生网络分区
+
+	currentNode := "rabbit@" + g.GetHost()
+	ov, err := funcs.GetOverview()
+	if err != nil {
+		log.Println(err.Error())
+		return
