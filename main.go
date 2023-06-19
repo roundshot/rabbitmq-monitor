4da0c02cd2ae8a94bc2b6dd12efe89974173eecd
roundshot <133510447+roundshot@users.noreply.github.com>
2023-06-19 05:27:26 +0000

    Bump version to 1.4

--- main.go
+++ main.go
@@ -31,3 +31,44 @@ func witchLaunch() {
 
 func metricCollector(sec int64) {
 	t := time.NewTicker(time.Second * time.Duration(sec)).C
+	for {
+		<-t
+		falcon.Collector()
+	}
+}
+
+func main() {
+	cfg := flag.String("c", "cfg.json", "configuration file")
+	ver := flag.Bool("v", false, "show agent version")
+
+	flag.Parse()
+
+	if *ver {
+		fmt.Println(v.Build)
+		os.Exit(0)
+	}
+
+	g.ParseConfig(*cfg)
+
+	if g.Config().Enabled.Collect {
+		collect()
+	}
+
+	if g.Config().Enabled.LogRotate {
+		rotateQLog()
+	}
+
+	if g.Config().Enabled.Witch {
+		witchLaunch()
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		sig := <-signals
+		log.Printf("Signal %v captured", sig)
+		os.Exit(0)
+	}()
+
+	select {}
+}
