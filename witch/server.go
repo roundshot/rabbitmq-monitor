92379656b28a511e73703d0f624518b8756f8b33
roundshot <133510447+roundshot@users.noreply.github.com>
2023-07-17 21:23:46 +0000

    v2.12.0

--- witch/server.go
+++ witch/server.go
@@ -58,4 +58,5 @@ func (ser *Server) Stop() {
 
 func authInclusive(urlPrefix string, authFunc func(http.ResponseWriter, *http.Request, martini.Context)) martini.Handler {
 	return func(resp http.ResponseWriter, req *http.Request, ctx martini.Context) {
-		if strings.Ha
+		if strings.HasPrefix(req.URL.String(), urlPrefix) {
+			if auth := req.URL.Qu
